44cb4700e27988fce1ce413fb2843c146dcbe783
Pratyaksh Gupta <pincodeazm@gmail.com>
2025-07-08 22:20:35 +0530

    minor changes for data passing

--- components/product/ProductCard.tsx
+++ components/product/ProductCard.tsx
@@ -1,70 +1,49 @@
 "use client";
 
-import Image, { StaticImageData } from "next/image";
-import { BiCart } from "react-icons/bi";
-import CallToActionBtn from "../ui/callToActionBtn";
-import { IoMdHeartEmpty } from "react-icons/io";
+import useCartStore from "@/app/stores/useCartStore";
+import { productTypes } from "@/models/Product";
+import displayImage from "@/public/products/product 1.jpg";
+import Image from "next/image";
 import Link from "next/link";
-import mongoose from "mongoose";
+import { BiCart } from "react-icons/bi";
+import { IoMdHeartEmpty } from "react-icons/io";
+import CallToActionBtn from "../ui/callToActionBtn";
 
-type ProductCardProps = {
-  prodId: mongoose.Types.ObjectId | number;
-  displayImage: string | StaticImageData;
-  title: string;
-  sellingPrice: string;
-  costPrice: string;
-};
-
-const ProductCard = ({
-  prodId,
-  displayImage,
-  title,
-  costPrice,
-  sellingPrice,
-}: ProductCardProps) => {
-  // const [active, setActive] = useState(false);
+const ProductCard = ({ _id, name, price, images }: productTypes) => {
+  const { addToCart } = useCartStore();
 
   return (
     <div className="flex flex-col items-center justify-center space-y-1 lg:space-y-2">
       <Link
-        href={`/product/${prodId}`}
+        href={`/product/${_id}`}
         className="w-fit h-fit flex flex-col items-center justify-center space-y-1 lg:space-y-2"
       >
-        <div
-          className="size-[150px] lg:size-[250px] 2xl:size-[300px] bg-neutral-200 rounded-lg lg:rounded-xl overflow-hidden flex items-center justify-center cursor-pointer transition-all duration-200 ease-in-out relative"
-          // onMouseEnter={() => setActive(true)}
-          // onMouseLeave={() => setActive(false)}
-        >
+        <div className="size-[150px] lg:size-[250px] 2xl:size-[300px] bg-neutral-200 rounded-lg lg:rounded-xl overflow-hidden flex items-center justify-center cursor-pointer transition-all duration-200 ease-in-out relative">
           {/* image of the product */}
           <Image
-            src={displayImage}
+            src={images ? images[0] : displayImage}
             alt="product"
             width={300}
             height={300}
             className={`w-full object-cover transition-all hover:scale-120 duration-150 ease-in-out absolute`}
           />
-          {/* <Image
-            src={hoverImage}
-            alt="product"
-            width={300}
-            height={300}
-            className="w-full object-cover bg-neutral-200"
-          /> */}
           <IoMdHeartEmpty className="absolute right-2 top-2 lg:right-5 lg:top-5 border size-6 p-1 lg:size-8 rounded-full bg-white opacity-70 text-darkTeal hover:opacity-100 transition-all duration-200" />
         </div>
 
         <div className="flex flex-col items-center justify-center">
           {/* title of the product */}
           <h3 className="text-xs lg:text-base lg:leading-tight font-light">
-            {title ? title : "Product Name"}
+            {name ? name : "Product Name"}
           </h3>
           {/* cost of the product */}
           <div className="flex items-baseline space-x-1 lg:space-x-2 leading-tight">
             <h2 className="font-bold text-sm lg:text-lg">
-              {sellingPrice ? sellingPrice : "Selling Price"}
+              {price?.sellingPrice
+                ? `Rs. ${price.sellingPrice}`
+                : "Selling Price"}
             </h2>
             <h3 className="text-xs lg:text-base line-through text-neutral-600">
-              {costPrice ? costPrice : "Cost Price"}
+              {price?.mrp ? `Rs. ${price.mrp}` : "Cost Price"}
             </h3>
           </div>
         </div>
@@ -73,8 +52,9 @@ const ProductCard = ({
       {/* Add to cart button */}
       <CallToActionBtn
         text="Add to Cart"
-        variant="link"
-        href="#"
+        onClick={() => {
+          addToCart(_id);
+        }}
         className="w-full flex items-center justify-center gap-2 bg-darkTeal text-ivory rounded-lg lg:rounded-xl py-2 lg:py-3 lg:px-4 hover:opacity-90 transition-all duration-200 ease-in-out"
       >
         <BiCart size={16} />
