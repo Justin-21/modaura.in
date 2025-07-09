cedf2e52b29645e60e761bff8ac7eec1b2a3a403
Pratyaksh Gupta <pincodeazm@gmail.com>
2025-07-09 20:37:57 +0530

    created productByCategory page

--- app/productByCategory/[id]/page.tsx
+++ app/productByCategory/[id]/page.tsx
@@ -1,14 +1,42 @@
 "use client";
 
+import useProductStore from "@/app/stores/useProductStore";
+import ProductCard from "@/components/product/ProductCard";
 import React, { use } from "react";
 
 const CategoryProduct = ({ params }: { params: Promise<{ id: string }> }) => {
   const { id } = use(params);
 
+  const { products } = useProductStore();
+
+  const items = products.filter(
+    (product) => product.category.toUpperCase() === id.toUpperCase()
+  );
+
   return (
-    <>
-      <div className="h-dvh">ProductByCategory Page {id}</div>
-    </>
+    <main className="w-full h-full space-y-5 lg:space-y-10">
+      <h3 className="text-3xl lg:text-5xl">{id}s</h3>
+
+      <div className="w-full text-end lg:text-lg">{items.length} products</div>
+
+      <div className="grid grid-cols-2 lg:grid-cols-5 items-center justify-items-center gap-5 lg:gap-10">
+        {items.map((item) => (
+          <ProductCard
+            key={item._id.toString()}
+            _id={item._id}
+            name={item.name}
+            images={item.images}
+            price={item.price}
+            category={""}
+            description={""}
+          />
+        ))}
+      </div>
+
+      <div className="">
+        Stay Tuned With Us. We are bringing a lot of new collections and items.
+      </div>
+    </main>
   );
 };
 
