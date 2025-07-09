8e0e239ee20fb1cc241fdae6672ae9665ceff5ba
Pratyaksh Gupta <pincodeazm@gmail.com>
2025-07-09 21:15:37 +0530

    fixed category page issues

--- components/product/ProductCard.tsx
+++ components/product/ProductCard.tsx
@@ -13,26 +13,26 @@ const ProductCard = ({ _id, name, price, images }: productTypes) => {
   const { addToCart } = useCartStore();
 
   return (
-    <div className="flex flex-col items-center justify-center space-y-1 lg:space-y-2">
+    <div className="w-full h-full flex flex-col items-center justify-center space-y-1 lg:space-y-2">
       <Link
         href={`/product/${_id}`}
-        className="w-fit h-fit flex flex-col items-center justify-center space-y-1 lg:space-y-2"
+        className="w-full h-full flex flex-col items-center justify-center space-y-1 lg:space-y-2"
       >
-        <div className="size-[180px] lg:size-[250px] 2xl:size-[300px] bg-neutral-200 rounded-lg lg:rounded-xl overflow-hidden flex items-center justify-center cursor-pointer transition-all duration-200 ease-in-out relative">
+        <div className="size-[calc(100dvw/2-6vw)] md:size-[200px] lg:size-[250px] 2xl:size-[300px] bg-neutral-200 rounded-lg lg:rounded-xl overflow-hidden flex items-center justify-center cursor-pointer transition-all duration-200 ease-in-out relative">
           {/* image of the product */}
           <Image
             src={images ? images[0] : displayImage}
             alt="product"
             width={300}
             height={300}
-            className={`w-full object-cover transition-all hover:scale-120 duration-150 ease-in-out absolute`}
+            className={`object-cover transition-all hover:scale-120 duration-150 ease-in-out absolute`}
           />
           <IoMdHeartEmpty className="absolute right-2 top-2 lg:right-5 lg:top-5 border size-6 p-1 lg:size-8 rounded-full bg-white opacity-70 text-darkTeal hover:opacity-100 transition-all duration-200" />
         </div>
 
         <div className="flex flex-col items-center justify-center">
           {/* title of the product */}
-          <h3 className="text-sm lg:text-base lg:leading-tight font-light">
+          <h3 className="text-sm lg:text-base lg:leading-tight font-light truncate">
             {name ? name : "Product Name"}
           </h3>
           {/* cost of the product */}
